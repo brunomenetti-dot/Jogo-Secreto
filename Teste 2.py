05237bb59b6b79448fc9fa8b4c03462ea111fa47
Bruno <bruno.menetti96@gmail.com>
2026-02-03 18:04:50 -0300

    Limpando a pasta

--- Teste 2.py
+++
@@ -1,2 +0,0 @@
-name="Jack"
-print('Hello World', name)
--- Teste String.py
+++
@@ -1,5 +0,0 @@
-url = input("Digite a URL para validação: ") 
-if url.startswith("https://") and url.endswith(".com"):
-    print("URL válida!")
-else:
-    print("URL inválida!")
